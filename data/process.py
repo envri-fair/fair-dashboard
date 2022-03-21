c2366a0a7bd6d195c597e06fcad17f5dbdbf3713
Markus Stocker <markus.stocker@gmail.com>
2022-03-21 16:34:58 +0100

    first draft with new fairness assessment data

--- data/process.py
+++ data/process.py
@@ -3,7 +3,101 @@ import json
 with open('d1.json', 'r') as f:
   d1 = json.load(f)
 
-d2 = {'Communities': [], 'Years': [], 'F1': {}, 'F2': {}, 'F3': {}, 'F4': {}, 'A1.1': {}, 'A1.2': {}, 'A2': {}, 'I1': {}, 'I2': {}, 'I3': {}, 'R1.1': {}, 'R1.2': {}, 'R1.3': {}}
+d2 = {'Communities': [], 
+    'Years': [], 
+    'Principles': {
+        'F1': {
+            'uri': 'https://www.go-fair.org/fair-principles/f1-meta-data-assigned-globally-unique-persistent-identifiers/',
+            'label': 'F1',
+            'definition': '(Meta) data are assigned globally unique and persistent identifiers'
+        },
+        'F2': {
+            'uri': 'https://www.go-fair.org/fair-principles/f2-data-described-rich-metadata/',
+            'label': 'F2',
+            'definition': 'Data are described with rich metadata'
+        },
+        'F3': {
+            'uri': 'https://www.go-fair.org/fair-principles/f3-metadata-clearly-explicitly-include-identifier-data-describe/',
+            'label': 'F3',
+            'definition': 'Metadata clearly and explicitly include the identifier of the data they describe'
+        },
+        'F4': {
+            'uri': 'https://www.go-fair.org/fair-principles/f4-metadata-registered-indexed-searchable-resource/',
+            'label': 'F4',
+            'definition': '(Meta)data are registered or indexed in a searchable resource'
+        },
+        'A1.1': {
+            'uri': 'https://www.go-fair.org/fair-principles/a1-1-protocol-open-free-universally-implementable/',
+            'label': 'A1.1',
+            'definition': 'The protocol is open, free and universally implementable'
+        },
+        'A1.2': {
+            'uri': 'https://www.go-fair.org/fair-principles/a1-2-protocol-allows-authentication-authorisation-required/',
+            'label': 'A1.2',
+            'definition': 'The protocol allows for an authentication and authorisation where necessary'
+        },
+        'A2': {
+            'uri': 'https://www.go-fair.org/fair-principles/a2-metadata-accessible-even-data-no-longer-available/',
+            'label': 'A2',
+            'definition': 'Metadata should be accessible even when the data is no longer available'
+        },
+        'I1': {
+            'uri': 'https://www.go-fair.org/fair-principles/i1-metadata-use-formal-accessible-shared-broadly-applicable-language-knowledge-representation/',
+            'label': 'I1',
+            'definition': '(Meta)data use a formal, accessible, shared, and broadly applicable language for knowledge representation'
+        },
+        'I2': {
+            'uri': 'https://www.go-fair.org/fair-principles/i2-metadata-use-vocabularies-follow-fair-principles/',
+            'label': 'I2',
+            'definition': '(Meta)data use vocabularies that follow the FAIR principles'
+        },
+        'I3': {
+            'uri': 'https://www.go-fair.org/fair-principles/i3-metadata-include-qualified-references-metadata/',
+            'label': 'I3',
+            'definition': '(Meta)data include qualified references to other (meta)data'
+        },
+        'R1.1': {
+            'uri': 'https://www.go-fair.org/fair-principles/r1-1-metadata-released-clear-accessible-data-usage-license/',
+            'label': 'R1.1',
+            'definition': '(Meta)data are released with a clear and accessible data usage license'
+        },
+        'R1.2': {
+            'uri': 'https://www.go-fair.org/fair-principles/r1-2-metadata-associated-detailed-provenance/',
+            'label': 'R1.2',
+            'definition': '(Meta)data are associated with detailed provenance'
+        },
+        'R1.3': {
+            'uri': 'https://www.go-fair.org/fair-principles/r1-3-metadata-meet-domain-relevant-community-standards/',
+            'label': 'R1.3',
+            'definition': '(Meta)data meet domain-relevant community standards'
+        }
+    },
+    'Demonstrators': {
+        'I1': [{
+            'label': 'SIOS netCDF-LD demonstrator',
+            'creator': 'Lara Ferrighi',
+            'date': '2020-04-29',
+            'url': 'https://mybinder.org/v2/gh/ferrighi/netcdf-ld-prototype.git/HEAD'
+        }],
+        'I2': [{
+            'label': 'BODC vocabularies demonstrator',
+            'creator': 'Alexandra Kokkinaki',
+            'date': '2020-01-23',
+            'url': 'https://mybinder.org/v2/gh/envri-fair/vocabularies-demonstrator.git/master'
+        }],
+        'R1.2': [{
+            'label': 'IAGOS provenance demonstrator',
+            'creator': 'Damien Boulanger',
+            'date': '2020-12-11',
+            'url': 'https://github.com/envri-fair/iagos-prov'
+        },{
+            'label': 'ACTRIS provenance demonstrator',
+            'creator': 'Markus Stocker',
+            'date': '2019-12-06',
+            'url': 'https://mybinder.org/v2/gh/envri-fair/provenance-demonstrator.git/master'
+        }]
+    },
+    'F1': {}, 'F2': {}, 'F3': {}, 'F4': {}, 'A1.1': {}, 'A1.2': {}, 'A2': {}, 'I1': {}, 'I2': {}, 'I3': {}, 'R1.1': {}, 'R1.2': {}, 'R1.3': {}}
 
 for binding in d1['results']['bindings']:
     p = binding['p']['value']
@@ -11,6 +105,9 @@ for binding in d1['results']['bindings']:
     y = binding['y']['value']
     t = binding['t']['value']
     r = binding['rel']['value'] # https://w3id.org/fair/fip/terms/declares-current-use-of, https://w3id.org/fair/fip/terms/declares-planned-use-of
+
+    if y ==  '2022': 
+        continue
 
     if c not in d2['Communities']:
         d2['Communities'].append(c)
@@ -36,6 +133,21 @@ for binding in d1['results']['bindings']:
             y_data = 0
 
     c_data[y] = y_data
+
+for community in d2['Communities']:
+    if community not in d2['R1.3']:
+        d2['R1.3'][community] = {} 
+    for year in d2['Years']:
+        if year not in d2['R1.3'][community]:
+            d2['R1.3'][community][year] = None
+        for principle in d2['Principles'].keys():
+            if community in d2[principle] and year in d2[principle][community]:
+                if d2['R1.3'][community][year] == None:
+                    d2['R1.3'][community][year] = d2[principle][community][year]
+                elif d2['R1.3'][community][year] == -1 and d2[principle][community][year] != -1:
+                    d2['R1.3'][community][year] = 0
+                elif d2['R1.3'][community][year] == 1 and d2[principle][community][year] != 1:
+                    d2['R1.3'][community][year] = 0
 
 with open('d2.json', 'w') as f:
   json.dump(d2, f, indent = 2)
--- src/App.js
+++ src/App.js
@@ -1,111 +1,77 @@
+import data from './data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { Table } from 'reactstrap';
-import * as axios from 'axios';
-import shortid from 'shortid';
-
-const base_url = "https://envri-fair.lab.uvalight.net/sparql";
+import { Table, Button } from 'reactstrap';
 
 function PrincipleTitle(props) {
   return (<h2><a href={props.uri}>{props.label}</a>: {props.definition}</h2>);
 }
 
+function PrincipleDemonstrators(props) {
+  if (data['Demonstrators'][props.principle] == undefined)
+    return (null);
+  
+  console.log(props.principle)
+
+  return (<div>
+    { 
+      data['Demonstrators'][props.principle].map(demonstrator => 
+        <><a href={demonstrator['url']}><Button outline color="secondary" className="my-4">{demonstrator['label']}</Button></a>{' '}</>
+      )
+    }
+  </div>);
+}
+
 function PrincipleTableHeader() {
-  return (<thead><tr><th width="40%">Infrastructure</th><th width="40%">Repository</th><th>Year 1</th><th>Year 2</th><th>Year 3</th></tr></thead>);
+  return (<thead><tr><th width="60%">Infrastructure</th><th>2019</th><th>2020</th><th>2021</th></tr></thead>);
 }
 
 function PrincipleTableRow(props) {
-  return (<tr><td>{props.infrastructure}</td><td>{props.repository}</td><td className={props.year1}>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>); 
-}
-
-function getQuery(uri) {
-  if (uri === 'https://w3id.org/fair/principles/terms/I1') {
-    return "prefix ns: <http://envri.eu/ns/> \
-            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
-            prefix skos: <http://www.w3.org/2004/02/skos/core#> \
-            select ?infrastructure ?repository ?value \
-            where { \
-              [] a ns:FAIRAssessment ; \
-              ns:infrastructure [ skos:prefLabel ?infrastructure ] ; \
-              ns:hasRepository [ \
-              rdfs:label ?repository ; \
-              ns:hasMetadata [ ns:isMachineActionable ?value ] \
-              ] \
-              FILTER (?value = ns:planned || ?value = 'false'^^xsd:bool || ?value='true'^^xsd:bool) \
-            }";
-  }
-
-  if (uri === 'https://w3id.org/fair/principles/terms/I2') {
-    return "prefix ns: <http://envri.eu/ns/> \
-            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
-            prefix skos: <http://www.w3.org/2004/02/skos/core#> \
-            select ?infrastructure ?repository ?value \
-            where { \
-              [] a ns:FAIRAssessment ; \
-              ns:infrastructure [ skos:prefLabel ?infrastructure ] ; \
-              ns:hasRepository [ \
-              rdfs:label ?repository ; \
-              ns:hasMetadata [ ns:categoriesAreDefinedInRegistries ?value ] \
-              ] \
-              FILTER (?value = ns:planned || ?value = 'false'^^xsd:bool || ?value='true'^^xsd:bool) \
-            }";
-  }
-
-  if (uri == 'https://w3id.org/fair/principles/terms/R1.2') {
-    return "prefix ns: <http://envri.eu/ns/> \
-            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
-            prefix skos: <http://www.w3.org/2004/02/skos/core#> \
-            select ?infrastructure ?repository ?value \
-            where { \
-              [] a ns:FAIRAssessment ; \
-              ns:infrastructure [ skos:prefLabel ?infrastructure ] ; \
-              ns:hasRepository [ \
-              rdfs:label ?repository ; \
-              ns:hasMetadata [ ns:hasMachineReadableProvenance ?value ] \
-              ] . \
-              FILTER (?value = 'false'^^xsd:bool || ?value='true'^^xsd:bool) \
-            }";
-  }
-
-  return null;
-}
-
-function GetData(uri) {
-  const query = getQuery(uri);
-
-  var url = base_url + "?query=" + encodeURIComponent(query) + "&format=json";
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    async function retrieve() {
-      const response = await axios.get(url);
-      setData(response.data.results.bindings);
-    }
-    retrieve();
-  }, []);
-
-  return data;
+  return (<tr><td>{props.infrastructure}</td>
+    <td className= 
+    {
+      props.year1 === -1 ? "red"
+      : props.year1 === 0 ? "yellow"
+      : props.year1 === 1 ? "green"
+      : "&nbsp;" 
+    }></td>
+    <td className=
+    {
+      props.year2 === -1 ? "red"
+      : props.year2 === 0 ? "yellow"
+      : props.year2 === 1 ? "green"
+      : "&nbsp;" 
+    }></td>
+    <td className=
+    {
+      props.year3 === -1 ? "red"
+      : props.year3 === 0 ? "yellow"
+      : props.year3 === 1 ? "green"
+      : "&nbsp;" 
+    }></td></tr>); 
 }
 
 function Principle(props) {
-  const data = GetData(props.uri);
-
-  if (!data) return null;
-
   return (
     <div>
-      <PrincipleTitle uri={props.uri} label={props.label} definition={props.definition} />
+      <PrincipleTitle uri={data.Principles[props.principle].uri} 
+                      label={data.Principles[props.principle].label} 
+                      definition={data.Principles[props.principle].definition} />
+      <PrincipleDemonstrators principle={props.principle} />
       <Table hover size="sm">
       <PrincipleTableHeader />
       <tbody>
-        { data.map(data => 
-          <PrincipleTableRow key={shortid.generate()}
-            infrastructure={data.infrastructure.value} 
-            repository={data.repository.value}
-            year1={data.value.value === 'true' ? 'green' : 'red'} />
-          ) }
+        { 
+          Object.keys(data[props.principle]).map((infrastructure, i) => ( 
+            <PrincipleTableRow key={i}
+              infrastructure={infrastructure} 
+              year1={data[props.principle][infrastructure]['2019']} 
+              year2={data[props.principle][infrastructure]['2020']}
+              year3={data[props.principle][infrastructure]['2021']}
+              />
+          ) )
+        }
       </tbody>
       </Table>
     </div>
@@ -113,103 +79,15 @@ function Principle(props) {
 }
 
 function App() {
-  var query = "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
-  prefix skos: <http://www.w3.org/2004/02/skos/core#> \
-  prefix fair: <https://w3id.org/fair/principles/terms/> \
-  select distinct ?fair_principle ?principle_label ?principle_definition \
-  where { \
-    ?fair_principle a fair:FAIR-SubPrinciple . \
-    ?fair_principle rdfs:label ?principle_label . \
-    ?fair_principle skos:definition ?principle_definition . \
-    FILTER (lang(?principle_label) = 'en' && lang(?principle_definition) = 'en') \
-  }";
-
-  var url = base_url + "?query=" + encodeURIComponent(query) + "&format=json";
-
-  const [principles, setPrinciples] = useState(null);
-
-  useEffect(() => {
-    async function getPrinciples() {
-      const response = await axios.get(url);
-      const principles = response.data.results.bindings.map(binding => ( { 
-        uri: binding.fair_principle.value,
-        label: binding.principle_label.value,
-        definition: binding.principle_definition.value
-      } ));
-      console.log(principles);
-      setPrinciples(principles);
-    }
-    getPrinciples();
-  }, []);
-
-  if (!principles) return null;
-
   return (
     <div className="content">
       <div className="header">
         <h1>FAIR Dashboard</h1>
       </div>
       <div className="main">
-        <Principle 
-          uri={principles.find(item => item.label === "F1").uri} 
-          label={principles.find(item => item.label === "F1").label} 
-          definition={principles.find(item => item.label === "F1").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "F2").uri} 
-          label={principles.find(item => item.label === "F2").label} 
-          definition={principles.find(item => item.label === "F2").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "F3").uri} 
-          label={principles.find(item => item.label === "F3").label} 
-          definition={principles.find(item => item.label === "F3").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "F4").uri} 
-          label={principles.find(item => item.label === "F4").label} 
-          definition={principles.find(item => item.label === "F4").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "A1").uri} 
-          label={principles.find(item => item.label === "A1").label} 
-          definition={principles.find(item => item.label === "A1").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "A1.1").uri} 
-          label={principles.find(item => item.label === "A1.1").label} 
-          definition={principles.find(item => item.label === "A1.1").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "A1.2").uri} 
-          label={principles.find(item => item.label === "A1.2").label} 
-          definition={principles.find(item => item.label === "A1.2").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "A2").uri} 
-          label={principles.find(item => item.label === "A2").label} 
-          definition={principles.find(item => item.label === "A2").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "I1").uri} 
-          label={principles.find(item => item.label === "I1").label} 
-          definition={principles.find(item => item.label === "I1").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "I2").uri} 
-          label={principles.find(item => item.label === "I2").label} 
-          definition={principles.find(item => item.label === "I2").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "I3").uri} 
-          label={principles.find(item => item.label === "I3").label} 
-          definition={principles.find(item => item.label === "I3").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "R1").uri} 
-          label={principles.find(item => item.label === "R1").label} 
-          definition={principles.find(item => item.label === "R1").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "R1.1").uri} 
-          label={principles.find(item => item.label === "R1.1").label} 
-          definition={principles.find(item => item.label === "R1.1").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "R1.2").uri} 
-          label={principles.find(item => item.label === "R1.2").label} 
-          definition={principles.find(item => item.label === "R1.2").definition} /> 
-        <Principle 
-          uri={principles.find(item => item.label === "R1.3").uri} 
-          label={principles.find(item => item.label === "R1.3").label} 
-          definition={principles.find(item => item.label === "R1.3").definition} /> 
+        {
+          Object.keys(data.Principles).map((principle, i) => ( <Principle key={i} principle={principle} /> ))
+        }  
       </div>
     </div>
   );
